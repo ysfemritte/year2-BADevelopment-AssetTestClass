d2b24d61baffc1f6eaf2f9f3b36f0cd8b12010a7
Yusuf Emritte <34180742+ysfemritte@users.noreply.github.com>
2021-09-17 18:19:03 +0400

    Add files via upload

---
+++ AssetTest/Asset.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTest
+{
+    class Asset
+    {
+        //1. IV - private var - associated with Implemented Properties
+        private double assetValue, depRate;
+
+
+        //2. Auto-Implemented Properties - Do not require VALIDATION
+        public string AssetName { get; set; }
+
+        //3. Implemented Properties - Require Validation
+        public double AssetValue
+        {
+            get 
+            {
+                return assetValue;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    assetValue = value;
+                }
+            }
+        }
+
+        public double DepRate
+        {
+            get
+            {
+                return depRate;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    depRate = value;
+                }
+            }
+        }
+
+        //4. Parameterised Constructor - Public Asset(Properties)
+        public Asset(string _assetName, double _assetValue, double _depRate)
+        {
+            //Properties = Parameters
+            AssetName = _assetName;
+            AssetValue = _assetValue;
+            DepRate = _depRate;
+        }
+
+        
+        //5. Methods
+        public double AmtDepreciation()
+        {
+            return AssetValue * DepRate / 100;
+        }
+
+        public double ValueDepreciation()
+        {
+            return AssetValue - AmtDepreciation();
+        }
+        //End AssetClass
+
+    }
+}
---
+++ AssetTest/AssetTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AssetTest
+{
+    class AssetTest
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 4.11: The Asset Class
+            //Create a class called Asset that includes 3 pieces of information 
+            //as either instance variables or auto-implemented properties.
+
+            //1. asset name (type string)
+            //2. asset value (type double , I prefer doubles to decimals)
+            //3. depreciation rate (type double)
+
+            //Your class should have a constructor that initializes the values.
+            //Provide a property with a get and set accessor for any instance variable.
+            //If the asset value or depreciation rate is negative, the values should remain unchanged.
+            //Write a test app called AssetTest that test the class. 
+            //Create two objects of Asset and test the class features.
+
+
+            //1. Create an Object
+            Asset asset1 = new Asset("Hotel", 150000, 10);
+
+            //Accessing Object Properties
+            Console.WriteLine($"Asset 1 is a {asset1.AssetName}, it's value is {asset1.AssetValue} \n ");
+
+            //Access the Methods
+            Console.WriteLine($"The depreciation amount is {asset1.AmtDepreciation()} \n ");
+            Console.WriteLine($"The value after depreciation is {asset1.ValueDepreciation()}");
+
+            Console.ReadLine();
+        }
+    }
+}
